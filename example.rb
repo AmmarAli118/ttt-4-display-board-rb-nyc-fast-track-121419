01c1b8f1ff0f71015c5df04f50a953694dd18094
Ammar Ali <ammar.ali@flatironschool.com>
2019-12-15 15:03:36 +0000

    Automatically backed up by Learn

---
+++ example.rb
@@ -0,0 +1,31 @@
+x = 140
+
+def new_function()
+  
+  # How can we edit this function to work based on user input?
+  
+  x = 
+  
+  if x < 20 || x.even?
+    if x > 5 && x.odd?
+      puts "not so big"
+    elsif x == 3 
+      puts "this is a magic number"
+    else 
+      puts "nothing"
+    end
+  else
+    puts "not less than 20, and not even"
+  end
+  
+  
+  puts "x equals = #{x}"
+  
+end 
+
+# new_function(9)
+
+puts "Enter your name: "
+name = gets
+puts "Hello, " + name
+
